51fe7241fefabb282ef192eecf8c9e67d89cbcc7
Robert812218 <robert812@shaw.ca>
2022-12-17 12:23:37 -0700

    setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+        <div className="content">
+              <h1>Under Construction</h1>
+	            <h3>This website is currently under construction and will launch December 25th 2022! Please fill out the form to receive the files.</h3>
+
+            <form action="https://formspree.io/f/mpzeaqjb" method="POST" className="form-container">
+              <input type="text" name="Name" placeholder="Full Name" required />
+              <input type="text" name="Email" placeholder="Email" required />
+              <textarea name="Message" placeholder="Message" required></textarea>
+              <button type="submit" className="button">Send</button>
+            </form>
+        </div>
     </div>
   );
 }
